8344a5204a98006fe5c5ded0cce1886341f64914
Gilberto Bertin <gilberto@isovalent.com>
2022-02-01 11:11:18 +0100

    k8s: keep CiliumNode labels synced with Node object
    This commit ensures that any update to the labels of a k8s Node object
    is reflected in the corresponding CiliumNode one.
    
    Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

--- pkg/k8s/watchers/node.go
+++ pkg/k8s/watchers/node.go
@@ -5,8 +5,11 @@ package watchers
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"sync"
 
+	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -16,10 +19,13 @@ import (
 	"k8s.io/client-go/tools/cache"
 
 	"github.com/cilium/cilium/pkg/comparator"
+	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
 	ciliumio "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 )
 
@@ -116,4 +122,76 @@ func (k *K8sWatcher) GetK8sNode(_ context.Context, nodeName string) (*v1.Node, e
 		}, nodeName)
 	}
 	return nodeInterface.(*v1.Node).DeepCopy(), nil
+}
+
+// CiliumNodeLabelsUpdater implements the subscriber.Node interface and is used
+// to keep CiliumNode objects labels in sync with the node ones.
+type CiliumNodeLabelsUpdater struct {
+	k8sWatcher *K8sWatcher
+}
+
+func NewCiliumNodeLabelsUpdater(k8sWatcher *K8sWatcher) *CiliumNodeLabelsUpdater {
+	return &CiliumNodeLabelsUpdater{
+		k8sWatcher: k8sWatcher,
+	}
+}
+
+func (u *CiliumNodeLabelsUpdater) OnAddNode(newNode *v1.Node, swg *lock.StoppableWaitGroup) error {
+	u.updateCiliumNodeLabels(newNode)
+	return nil
+}
+
+func (u *CiliumNodeLabelsUpdater) OnUpdateNode(oldNode, newNode *v1.Node, swg *lock.StoppableWaitGroup) error {
+	u.updateCiliumNodeLabels(newNode)
+	return nil
+}
+
+func (u *CiliumNodeLabelsUpdater) OnDeleteNode(*v1.Node, *lock.StoppableWaitGroup) error {
+	return nil
+}
+
+func (u *CiliumNodeLabelsUpdater) updateCiliumNodeLabels(node *v1.Node) {
+	var (
+		nodeName   = node.Name
+		nodeLabels = node.GetLabels()
+
+		controllerName = fmt.Sprintf("sync-node-labels-with-ciliumnode (%v)", nodeName)
+		scopedLog      = log.WithFields(logrus.Fields{
+			logfields.Controller: controllerName,
+			logfields.Node:       nodeName,
+		})
+	)
+
+	k8sCM.UpdateController(controllerName,
+		controller.ControllerParams{
+			DoFunc: func(ctx context.Context) (err error) {
+				u.k8sWatcher.ciliumNodeStoreMU.Lock()
+				if u.k8sWatcher.ciliumNodeStore == nil {
+					u.k8sWatcher.ciliumNodeStoreMU.Unlock()
+					return errors.New("CiliumNode cache store not yet initialized")
+				}
+				u.k8sWatcher.ciliumNodeStoreMU.Unlock()
+
+				ciliumNodeInterface, exists, err := u.k8sWatcher.ciliumNodeStore.GetByKey(nodeName)
+				if err != nil {
+					scopedLog.WithError(err).
+						Error("Failed to get CiliumNode resource from cache store")
+					return err
+				}
+
+				if !exists {
+					return nil
+				}
+
+				ciliumNode := ciliumNodeInterface.(*cilium_v2.CiliumNode).DeepCopy()
+				ciliumNode.Labels = nodeLabels
+
+				_, err = k8s.CiliumClient().CiliumV2().CiliumNodes().Update(ctx, ciliumNode, metav1.UpdateOptions{})
+				if err != nil {
+					scopedLog.WithError(err).
+						Error("Failed to update CiliumNode labels")
+				}
+				return err
+			},
+		})
 }
